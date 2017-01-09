e767ef3eada84132d77b2589892a76fadd0e3095
Gabriel Trintinalia <gabriel.trintinalia@gmail.com>
2017-01-10 10:53:57 +1100

    20171001- Removal of main tab

--- app/src/main/java/com/ziegler/hereiam/MainActivity.java
+++ app/src/main/java/com/ziegler/hereiam/MainActivity.java
@@ -2,31 +2,29 @@ package com.ziegler.hereiam;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.Query;
+import com.ziegler.hereiam.Models.RoomItemList;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private FloatingActionButton mFab;
     final Context context = this;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter<RoomItemListViewHolder> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +34,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.feeds_view_pager);
-        FeedsPagerAdapter adapter = new FeedsPagerAdapter(getSupportFragmentManager(), MainActivity.this);
-        adapter.addFragment(new ListRoomsFragment(), "UPCOMING");
-        adapter.addFragment(new ListRoomsFragment(), "UPCOMING");
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        mRecyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+
+        Query allPostsQuery = FirebaseUtil.getBaseRef().child("people").child("teste").child("rooms");
+        mAdapter = getFirebaseRecyclerAdapter(allPostsQuery);
+
+        mRecyclerView.setAdapter(mAdapter);
+
 
         // adapter.addFragment(new EarningsFragment(), "EARNINGS");
 
-
-        viewPager.setAdapter(adapter);
-        viewPager.setCurrentItem(0);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.feeds_tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
 
         mFab = (FloatingActionButton) findViewById(R.id.fab);
         mFab.setOnClickListener(new View.OnClickListener() {
@@ -87,53 +89,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    class FeedsPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-        private Context context;
-        private int[] imageResId = {
-                R.drawable.ic_main,
-                R.drawable.ic_main,
-                R.drawable.ic_main,
-                R.drawable.ic_main
+    private FirebaseRecyclerAdapter<RoomItemList, RoomItemListViewHolder> getFirebaseRecyclerAdapter(Query query) {
+        return new FirebaseRecyclerAdapter<RoomItemList, RoomItemListViewHolder>(
+                RoomItemList.class, R.layout.room_item_list, RoomItemListViewHolder.class, query) {
+            @Override
+            public void populateViewHolder(final RoomItemListViewHolder itemViewHolder,
+                                           final RoomItemList room, final int position) {
+                setupRoomItemList(itemViewHolder, room, position, null);
+            }
+
+            @Override
+            public void onViewRecycled(RoomItemListViewHolder holder) {
+                super.onViewRecycled(holder);
+//                FirebaseUtil.getLikesRef().child(holder.mPostKey).removeEventListener(holder.mLikeListener);
+            }
         };
-
-
-        public FeedsPagerAdapter(FragmentManager fm, Context context) {
-            super(fm);
-            this.context = context;
-        }
-
-        public FeedsPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-
-            Drawable image = ContextCompat.getDrawable(context, imageResId[position]);
-            image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
-            SpannableString sb = new SpannableString(" ");
-            ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
-            sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            return sb;
-        }
     }
 
 
+    private void setupRoomItemList(final RoomItemListViewHolder roomItemListViewHolder, final RoomItemList roomItemList, final int position, final String roomKey) {
+        roomItemListViewHolder.setName(roomItemList.getName());
+
+        roomItemListViewHolder.setOnClickListener(new RoomItemListViewHolder.RoomItemClickListener() {
+            @Override
+            public void openRoom() {
+                Intent room = new Intent(MainActivity.this, MapActivity.class);
+
+                String roomKey = ((FirebaseRecyclerAdapter) mAdapter).getRef(position).getKey();
+                room.putExtra(getString(R.string.EVENT_KEY), roomKey);
+
+                startActivity(room);
+            }
+        });
+
+    }
 }
